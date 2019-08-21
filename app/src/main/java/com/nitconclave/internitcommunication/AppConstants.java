899ba2c4d4cd0dacc76e9e0f836c5e1105ec5b4c
Ayush Dubey <2017UGCS079@nitjsr.ac.in>
2019-08-22 00:50:51 +0530

    Add email verification

--- app/src/main/java/com/nitconclave/internitcommunication/AppConstants.java
+++ app/src/main/java/com/nitconclave/internitcommunication/AppConstants.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AppConstants {
 
     private ArrayList<Drawable> logos;
+    private HashMap<String, String> shortForms;
 
     AppConstants(Context context) {
         logos = new ArrayList<>();
+        shortForms = new HashMap<>();
+
         logos.add(context.getDrawable(R.drawable.agartala));
         logos.add(context.getDrawable(R.drawable.allahabad));
         logos.add(context.getDrawable(R.drawable.andhra));
@@ -42,9 +46,46 @@ public class AppConstants {
         logos.add(context.getDrawable(R.drawable.trichy));
         logos.add(context.getDrawable(R.drawable.uttarakhand));
         logos.add(context.getDrawable(R.drawable.warangal));
+
+        shortForms.put("mnnit", "allahabad$2");
+        shortForms.put("manit", "bhopal$5");
+        shortForms.put("nitc", "calicut$6");
+        shortForms.put("nith", "hamirpur$10");
+        shortForms.put("mnit", "jaipur$11");
+        shortForms.put("nitj", "jalandhar$12");
+        shortForms.put("nitjsr", "jamshedpur$13");
+        shortForms.put("nitkkr", "kurukshetra$15");
+        shortForms.put("vnit", "nagpur$20");
+        shortForms.put("nitrkl", "rourkela$24");
+        shortForms.put("nits", "silchar$26");
+        shortForms.put("nitk", "karnataka$14");
+        shortForms.put("nitw", "warangal$31");
+        shortForms.put("nitdgp", "durgapur$8");
+        shortForms.put("nitsri", "srinagar$27");
+        shortForms.put("svnit", "surat$28");
+        shortForms.put("nitt", "trichy$29");
+        shortForms.put("nitp", "patna$21");
+        shortForms.put("nitrr", "raipur$23");
+        shortForms.put("nita", "agartala$1");
+        shortForms.put("nitap", "arunachal$4");
+        shortForms.put("nitd", "delhi$7");
+        shortForms.put("nitg", "goa$9");
+        shortForms.put("nitmn", "manipur$16");
+        shortForms.put("nitm", "meghalaya$17");
+        shortForms.put("nitmz", "mizoram$18");
+        shortForms.put("nitn", "nagaland$19");
+        shortForms.put("nitpy", "puducherry$22");
+        shortForms.put("nitskm", "sikkim$25");
+        shortForms.put("nituk", "uttarakhand$30");
+        shortForms.put("nitanp", "andhra$3");
+
     }
 
     public ArrayList<Drawable> getLogos() {
         return logos;
+    }
+
+    public HashMap<String, String> getShortForms() {
+        return shortForms;
     }
 }
--- app/src/main/java/com/nitconclave/internitcommunication/MainActivity.java
+++ app/src/main/java/com/nitconclave/internitcommunication/MainActivity.java
@@ -2,6 +2,7 @@ package com.nitconclave.internitcommunication;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.res.AssetManager;
 import android.graphics.Typeface;
@@ -9,7 +10,11 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -18,8 +23,14 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView mLoginText;
     private ProgressBar mLoginProgress;
     private TextView mSignUp;
+    private boolean mVerified;
+    private String mEmail;
+    private boolean mValidEmail;
+    private HashMap<String, String> mDomains;
+    private CoordinatorLayout mCoordinator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +68,62 @@ public class MainActivity extends AppCompatActivity {
         mLoginText = findViewById(R.id.button_text);
         mLoginProgress = findViewById(R.id.button_progress);
         mSignUp = findViewById(R.id.sign_up);
+        mVerified = false;
+        mDomains = mAppConstants.getShortForms();
+        mCoordinator = findViewById(R.id.coordinator);
 
+        mEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mValidEmail = !TextUtils.isEmpty(charSequence) && Patterns.EMAIL_ADDRESS.matcher(charSequence).matches();
+                mVerified = mValidEmail;
+                if (!mVerified) {
+                    mErrorImageView.setVisibility(View.VISIBLE);
+                    mErrorImageView.setImageDrawable(getDrawable(R.drawable.ic_error_black_24dp));
+                } else {
+                    mEmail = charSequence.toString();
+                    String chars[] = mEmail.split("@");
+                    String domain[] = chars[1].split("\\.");
+                    if (mDomains.containsKey(domain[0].toLowerCase())) {
+                        mVerified = true;
+                        String value = mDomains.get(domain[0].toLowerCase());
+                        String college[] = value.split("\\$");
+                        Log.e(LOG_TAG, college[0]);
+                        int co = Integer.parseInt(college[1].trim());
+                        mErrorImageView.setImageDrawable(mLogos.get(co - 1));
+                    } else {
+                        mErrorImageView.setVisibility(View.VISIBLE);
+                        mErrorImageView.setImageDrawable(getDrawable(R.drawable.ic_error_black_24dp));
+                        mVerified = false;
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mVerified)
+                    Snackbar.make(mCoordinator, "Invalid email address", Snackbar.LENGTH_LONG).show();
+                else {
+                    mLoginText.setVisibility(View.GONE);
+                    mLoginProgress.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+
+        
         updateLogo();
-
     }
 
     private void updateLogo() {
