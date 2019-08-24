1ecd9cc38ed57dfba3792e922f19cc6704402daa
Prajwal <2017ugcs059@nitjsr.ac.in>
2019-08-24 13:01:57 +0530

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/nitconclave/internitcommunication/Fragments/ChannelFragment.java
+++ app/src/main/java/com/nitconclave/internitcommunication/Fragments/ChannelFragment.java
@@ -5,19 +5,25 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.nitconclave.internitcommunication.Activities.DisplayActivity;
 import com.nitconclave.internitcommunication.Helpers.AppConstants;
+import com.nitconclave.internitcommunication.Models.User;
 import com.nitconclave.internitcommunication.R;
 
 
@@ -40,12 +46,31 @@ public class ChannelFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_channel, container, false);
         listView = view.findViewById(R.id.list);
         context = view.getContext();
-        appConstants = new AppConstants(context);
+        appConstants = new AppConstants(getContext());
 
+        User mUser = appConstants.getmUser();
         String id = appConstants.getmSecret();
+
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference();
-        // databaseReference.child("users").child("")
+        final String institute = appConstants.getCollege();
+        Log.e(institute, id);
+        databaseReference.child("users").child(institute).child(id).child("mList").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    String post = (String) postSnapshot.getValue();
+                    Log.e("Get Data", post);
+                    Log.e("Get Data", institute);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
         return view;
